80335cae8a2d2f9badd808499b5b2887fe02db80
root <root@mediaserver.ludwig.im>
2014-10-19 00:33:08 +0200

    Table resolver uninstall option

--- Disqus/_build/resolvers/resolve.tables.php
+++ Disqus/_build/resolvers/resolve.tables.php
@@ -9,6 +9,7 @@
 /* define package name */
 define('PKG_NAME','LudwigDisqus');
 define('PKG_NAME_LOWER',strtolower(PKG_NAME));
+define('PKG_TABLE_PREFIX', 'disqus_');
 
 if ($object->xpdo) {
     switch ($options[xPDOTransport::PACKAGE_ACTION]) {
@@ -16,15 +17,28 @@ if ($object->xpdo) {
 
             $modx =& $object->xpdo;
             $modelPath = $modx->getOption('core_path').'components/'. PKG_NAME_LOWER .'/model/';
-	    $modx->addPackage( PKG_NAME_LOWER, $modelPath, 'disqus_');
+	    $modx->addPackage( PKG_NAME_LOWER, $modelPath, PKG_TABLE_PREFIX);
 
             $manager = $modx->getManager();
             $manager->createObjectContainer('Comment');
             $manager->createObjectContainer('Thread');
-
             break;
+
         case xPDOTransport::ACTION_UPGRADE:
             break;
+
+	case xPDOTransport::ACTION_UNINSTALL:
+
+            $modx =& $object->xpdo;
+            $modelPath = $modx->getOption('core_path').'components/'. PKG_NAME_LOWER .'/model/';
+            if ( $modx->addPackage( PKG_NAME_LOWER, $modelPath, PKG_TABLE_PREFIX) )
+	    {
+            	$manager = $modx->getManager();
+	        $manager->removeObjectContainer('Comment');
+        	$manager->removeObjectContainer('Thread');
+	    }
+            break;
+
     }
 }
 return true;
