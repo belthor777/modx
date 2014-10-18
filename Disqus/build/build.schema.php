e71a792e04a3de6e5f1359380ab15541f8e362f3
gitolite <test@ludwig.im>
2014-10-18 10:55:56 +0200

    Prepared for Github

---
+++ Disqus/build/build.schema.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * LudwigDisqus
+ *
+ * Copyright 2014 by Thomas Ludwig <thomas@ludwig.im>
+ *
+ * LudwigDisqus is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ *
+ * LudwigDisqus is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * LudwigDisqus; if not, write to the Free Software Foundation, Inc., 59 Temple
+ * Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * @package ludwigdisqus
+ */
+/**
+ * Build Schema script
+ *
+ * @package ludwigdisqus
+ * @subpackage build
+ */
+$mtime = microtime();
+$mtime = explode(" ", $mtime);
+$mtime = $mtime[1] + $mtime[0];
+$tstart = $mtime;
+set_time_limit(0);
+
+/* define package name */
+define('PKG_NAME','LudwigDisqus');
+define('PKG_NAME_LOWER',strtolower(PKG_NAME));
+
+/* load modx and configs */
+require_once dirname(__FILE__) . '/build.config.php';
+
+/* define sources */
+$root = realpath(dirname(dirname(__FILE__)).'/../../../') .'/';
+$sources = array(
+    'model' => $root.'core/components/'.PKG_NAME_LOWER.'/model/',
+    'schema_file' => $root.'core/components/'.PKG_NAME_LOWER.'/schema/'.PKG_NAME_LOWER.'.mysql.schema.xml',
+);
+
+/* load modx and configs */
+include_once MODX_CORE_PATH . 'model/modx/modx.class.php';
+$modx= new modX();
+$modx->initialize('mgr');
+$modx->loadClass('transport.modPackageBuilder','',false, true);
+echo '<pre>'; /* used for nice formatting of log messages */
+$modx->setLogLevel(modX::LOG_LEVEL_INFO);
+$modx->setLogTarget('ECHO');
+
+$manager= $modx->getManager();
+$generator= $manager->getGenerator();
+
+$generator->classTemplate= <<<EOD
+<?php
+/**
+ * [+phpdoc-package+]
+ */
+class [+class+] extends [+extends+] {}
+?>
+EOD;
+$generator->platformTemplate= <<<EOD
+<?php
+/**
+ * [+phpdoc-package+]
+ */
+require_once (strtr(realpath(dirname(dirname(__FILE__))), '\\\\', '/') . '/[+class-lowercase+].class.php');
+class [+class+]_[+platform+] extends [+class+] {}
+?>
+EOD;
+$generator->mapHeader= <<<EOD
+<?php
+/**
+ * [+phpdoc-package+]
+ */
+EOD;
+
+if (!is_dir($sources['model'])) { $modx->log(modX::LOG_LEVEL_ERROR,'Model directory not found!'); die(); }
+if (!file_exists($sources['schema_file'])) { $modx->log(modX::LOG_LEVEL_ERROR,'Schema file not found!'); die(); }
+$generator->parseSchema( $sources['schema_file'], $sources['model'] );
+
+// Create Tables
+//$modx->addPackage( PKG_NAME_LOWER, $sources['model'], 'disqus_');
+//$manager->createObjectContainer('Comment'); // created the database table
+//$manager->createObjectContainer('Thread'); // created the database table
+
+$mtime= microtime();
+$mtime= explode(" ", $mtime);
+$mtime= $mtime[1] + $mtime[0];
+$tend= $mtime;
+$totalTime= ($tend - $tstart);
+$totalTime= sprintf("%2.4f s", $totalTime);
+
+echo "\nExecution time: {$totalTime}\n";
+
+exit ();
