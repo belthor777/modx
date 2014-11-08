76aeec8f2518f1bdaf6a26f4e2a9312ee8d25151
gitolite <test@ludwig.im>
2014-11-08 20:34:31 +0100

    Remove settings: not needed

--- QRCode/_build/data/transport.settings.php
+++ QRCode/_build/data/transport.settings.php
@@ -5,13 +5,4 @@
  */
 $settings = array();
 
-$settings['ludwigqrcode.activated']= $modx->newObject('modSystemSetting');
-$settings['ludwigqrcode.activated']->fromArray(array(
-     'key' => 'ludwigqrcode.activated',
-     'value' => '1',
-     'xtype' => 'combo-boolean',
-     'namespace' => 'ludwigqrcode',
-     'area' => 'general',
-),'',true,true);
-
 return $settings;
