d64a0c4516bb83cebe37dc131955100fd79b7661
gitolite <test@ludwig.im>
2014-11-09 02:32:00 +0100

    Added new chunk

--- QRCode/_build/data/transport.chunks.php
+++ QRCode/_build/data/transport.chunks.php
@@ -12,19 +12,41 @@ $chunks[$i]= $modx->newObject('modChunk');
 $chunks[$i]->fromArray(array(
     'id' => $i,
     'name' => 'qrcode_url',
-    'description' => 'Generates a QR-Code Link to your webpage: use [[$qrcode_url]]',
+    'description' => 'Generates a QR-Code Link to your webpage: [[$qrcode_url &link=`https://ludwig.im`]] or [[$qrcode_url]]',
     'snippet' => file_get_contents($sources['chunks'].'qrcode_url.chunk.tpl'),
     'properties' => '',
 ),'',true,true);
 
-// LudwigQRcode Image Chunk
+// LudwigQRcode Text Chunk
 $i++;
 $chunks[$i]= $modx->newObject('modChunk');
 $chunks[$i]->fromArray(array(
     'id' => $i,
-    'name' => 'qrcode_img',
-    'description' => 'Generates a QR-Code with your text: [[$qrcode_img? &text=`Hello World` &width=`80`]]',
-    'snippet' => file_get_contents($sources['chunks'].'qrcode_img.chunk.tpl'),
+    'name' => 'qrcode_text',
+    'description' => 'Generates a QR-Code with your text: [[$qrcode_text? &text=`Hello World`]]',
+    'snippet' => file_get_contents($sources['chunks'].'qrcode_text.chunk.tpl'),
+    'properties' => '',
+),'',true,true);
+
+// LudwigQRcode Phone Chunk
+$i++;
+$chunks[$i]= $modx->newObject('modChunk');
+$chunks[$i]->fromArray(array(
+    'id' => $i,
+    'name' => 'qrcode_phone',
+    'description' => 'Generates a QR-Code with your Phone number: [[$qrcode_phone? &num=`+49 171 5555555`]]',
+    'snippet' => file_get_contents($sources['chunks'].'qrcode_phone.chunk.tpl'),
+    'properties' => '',
+),'',true,true);
+
+// LudwigQRcode SMS Chunk
+$i++;
+$chunks[$i]= $modx->newObject('modChunk');
+$chunks[$i]->fromArray(array(
+    'id' => $i,
+    'name' => 'qrcode_sms',
+    'description' => 'Generates a QR-Code to send an SMS: [[$qrcode_sms? &num=`+49 171 5555555` &text=`Hello this is a test text message`]]',
+    'snippet' => file_get_contents($sources['chunks'].'qrcode_sms.chunk.tpl'),
     'properties' => '',
 ),'',true,true);
 
