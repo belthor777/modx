de1d6bfa062de3cbadb937d744219b08432c03ff
gitolite <test@ludwig.im>
2014-11-30 23:14:10 +0100

    Initial commit

--- Shariff/_build/data/transport.chunks.php
+++ Shariff/_build/data/transport.chunks.php
@@ -6,15 +6,4 @@
 $pkg = "ludwigshariff";
 $chunks = array();
 
-// Shariff Chunk
-$i = 0;
-$chunks[$i]= $modx->newObject('modChunk');
-$chunks[$i]->fromArray(array(
-    'id' => 0,
-    'name' => 'shariff',
-    'description' => 'Social share privacy buttons',
-    'snippet' => file_get_contents($sources['chunks'].'shariff.chunk.tpl'),
-    'properties' => '',
-),'',true,true);
-
 return $chunks;
--- Shariff/_build/data/transport.snippets.php
+++ Shariff/_build/data/transport.snippets.php
@@ -5,6 +5,25 @@
  * @subpackage build
  */
 $snippets = array();
+if (!function_exists('getSnippetContent')) {
+    function getSnippetContent($filename) {
+        $o = file_get_contents($filename);
+        $o = str_replace('<?php','',$o);
+        $o = str_replace('?>','',$o);
+        $o = trim($o);
+        return $o;
+    }
+}
+
+// Import Items
+$i= 0;
+$snippets[$i]= $modx->newObject('modSnippet');
+$snippets[$i]->fromArray(array(
+    'id' => $i,
+    'name' => 'shariff',
+    'description' => 'Social share privacy buttons',
+    'snippet' => getSnippetContent($sources['snippets'] . 'shariff.snippet.php'),
+),'',true,true);
 
 return $snippets;
 
