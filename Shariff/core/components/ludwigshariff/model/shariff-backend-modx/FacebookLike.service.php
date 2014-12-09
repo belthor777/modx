b65b225b5d643cde700f4b4c0424315187438bd7
gitolite <test@ludwig.im>
2014-12-10 00:30:26 +0100

    Added LinkedIn and FacebookLike

--- Shariff/core/components/ludwigshariff/model/shariff-backend-modx/FacebookLike.service.php
+++ Shariff/core/components/ludwigshariff/model/shariff-backend-modx/FacebookLike.service.php
@@ -1,23 +1,23 @@
 <?php
 
-class Facebook extends Requests implements ServiceInterface
+class FacebookLike extends Requests implements ServiceInterface
 {
 
     public function getName()
     {
-        return 'facebook';
+        return 'facebooklike';
     }
 
     public function getRequest($url)
     {
         $facebookURL = 'https://api.facebook.com/method/fql.query?';
         $facebookURL .= http_build_query( array('format' => 'json',
-                                                'query'  => 'select share_count from link_stat where url="' . $url . '"') );
+                                                'query'  => 'select like_count from link_stat where url="' . $url . '"') );
         return $this->createRequest($facebookURL);
     }
 
     public function extractCount($data)
     {
-        return $data[0]['share_count'];
+        return $data[0]['like_count'];
     }
 }
---
+++ Shariff/core/components/ludwigshariff/model/shariff-backend-modx/LinkedIn.service.php
@@ -0,0 +1,23 @@
+<?php
+
+class LinkedIn extends Requests implements ServiceInterface
+{
+
+    public function getName()
+    {
+        return 'linkedin';
+    }
+
+    public function getRequest($url)
+    {
+        $linkedinURL = 'https://www.linkedin.com/countserv/count/share?';
+        $linkedinURL .= http_build_query( array('url'  => $url,
+                                                'format' => 'json') );
+        return $this->createRequest($linkedinURL);
+    }
+
+    public function extractCount($data)
+    {
+        return $data[0]['count'];
+    }
+}
