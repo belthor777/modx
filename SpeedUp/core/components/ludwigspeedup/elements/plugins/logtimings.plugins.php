c814c4802e146bdd520121458323721faea0b5b3
gitolite <test@ludwig.im>
2014-12-22 21:52:40 +0100

    Added SpeedUp

--- SpeedUp/core/components/ludwigspeedup/elements/plugins/logtimings.plugins.php
+++ SpeedUp/core/components/ludwigspeedup/elements/plugins/logtimings.plugins.php
@@ -30,29 +30,46 @@
  * version 2 or (at your option) any later version for ludwigspeedup sourcecode
  * @package ludwigspeedup
  **/
-define('PKG_NAME', 'LudwigSpeedUP');
-define('PKG_NAME_LOWER', strtolower(PKG_NAME));
+$PKG_NAME= 'LudwigSpeedUP';
+$PKG_NAME_LOWER= strtolower($PKG_NAME);
 
 $e = &$modx->Event;
 if ($e->name == 'OnWebPageComplete')
 {
-	$id= $modx->resource->get('id');
-	$parse_time= number_format(round(microtime(true) - $modx->startTime, 7), 7);
-	$queries_time= number_format(round($modx->queryTime, 7), 7);
+	// Check if the Extra as well as LogTiming is enabled
+	$activated = array(	'extra' => $modx->getOption( $PKG_NAME_LOWER .'.activated'),
+								'logtiming' => $modx->getOption( $PKG_NAME_LOWER .'.logtiming_enabled'),
+								'plugin-cache' => $modx->getOption( $PKG_NAME_LOWER .'.plugin-cache_enabled')
+					);
 
-	$data = array(
-		'id' => $id,
-		'memory_peak' => floatval( memory_get_peak_usage(true) / 1048576 ),
-		//'from_modx_cache' => $modx->sourceCache, //_cacheFlag
-		'from_plugin_cache' => boolval( true ),
-		'total_queries' => intval( isset ($modx->executedQueries) ? $modx->executedQueries : 0 ),
-		'total_queries_time' => floatval( $queries_time ),
-		'total_parse_time' => floatval( $parse_time ),
-		'total' => floatval( $parse_time + $queries_time )
-	);
+	// Check if package is installed and activated
+	$ldpath = MODX_CORE_PATH.'components/'. $PKG_NAME_LOWER .'/model/';
+	if(!$modx->addPackage( $PKG_NAME_LOWER, $ldpath, $PKG_NAME_LOWER .'_') || !$activated['extra'] )
+	{
+		return('There was a problem adding the '. $PKG_NAME .' package!  Check the logs for more info!' );
 
-	$speedup_add = $modx->newObject('LogTimings', $data);
-	$speedup_add->save();
+	// Is LogTiming enabled?
+	} elseif ($activated['logtiming']) {
+
+		$id= $modx->resource->get('id');
+		$parse_time= number_format(round(microtime(true) - $modx->startTime, 7), 7);
+		$queries_time= number_format(round($modx->queryTime, 7), 7);
+
+		$data = array(
+			'id' => $id,
+			'memory_peak' => floatval( memory_get_peak_usage(true) / 1048576 ),
+			//'from_modx_cache' => $modx->sourceCache, //_cacheFlag
+			'from_plugin_cache' => boolval( $activated['plugin-cache'] ),
+			'total_queries' => intval( isset ($modx->executedQueries) ? $modx->executedQueries : 0 ),
+			'total_queries_time' => floatval( $queries_time ),
+			'total_parse_time' => floatval( $parse_time ),
+			'total' => floatval( $parse_time + $queries_time )
+		);
+
+		$speedup_add = $modx->newObject('LogTimings', $data);
+		$speedup_add->save();
+
+	}
 }
 
 return('');
