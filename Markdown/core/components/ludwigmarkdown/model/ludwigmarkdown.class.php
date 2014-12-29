8ea52a535620aca70957b10ae88ad9647b4383e1
gitolite <test@ludwig.im>
2014-12-29 06:26:45 +0100

    Added Submodule php-markdown and pandoc-php

--- Markdown/core/components/ludwigmarkdown/model/ludwigmarkdown.class.php
+++ Markdown/core/components/ludwigmarkdown/model/ludwigmarkdown.class.php
@@ -35,18 +35,20 @@ class LudwigMarkdown
 
 	public function __construct( modX &$modx )
 	{
-			$this->modx =& $modx;
+		$this->modx =& $modx;
 	}
 
 
 	// Extract Markdown from HTML
-	// Start Markdown: <pre id="markdown">
-	// End Markdown: </pre>
-	public function get_markdown( $output )
+	// - Insert css class for markdown
+	// - Start Markdown: <pre id="markdown">
+	// - End Markdown: </pre>
+	public function get_markdown( $content )
 	{
-		return( preg_replace(	"|<pre id=\"markdown\">(.*)</pre>|Uis",
-										"<article class=\"markdown\"><pre id=\"markdown\">$1</pre></article>",
-										$output) );	
+		$output= preg_replace(	"|<pre id=\"markdown\">(.*)</pre>|Uis",
+					"<article class=\"markdown\"><pre id=\"markdown\">$1</pre></article>",
+					$content);
+		return( $output );
 	}
 
 
@@ -59,20 +61,20 @@ class LudwigMarkdown
 
 		$pandoc = new Pandoc\Pandoc();
 		$options = array(	"from" => "markdown_github",
-								"to" => "html5",
-								"ascii" => null,
-								"normalize" => null,
-								"no-highlight" => null,
-								"email-obfuscation" => "none",
-								"from" => "markdown-markdown_in_html_blocks" );
+					"to" => "html5",
+					"ascii" => null,
+					"normalize" => null,
+					"no-highlight" => null,
+					"email-obfuscation" => "none",
+					"from" => "markdown-markdown_in_html_blocks" );
 
 		// Find all Markdown blocks
 		// and replace them with converted HTML5
-		preg_match_all( "|<pre id=\"markdown\">(.*)</pre>|Uis", $this->get_markdown( $output ), $matches );
+		$output= $this->get_markdown( $output );
+		preg_match_all( "|<pre id=\"markdown\">(.*)</pre>|Uis", $output, $matches );
 		foreach( $matches[0] as $key => $val )
 		{
-			$text = $pandoc->runWith($matches[1][$key], $options);
-			$output = str_replace($val,$text,$output);
+			$output = str_replace( $val, $pandoc->runWith($matches[1][$key], $options), $output );
 		}
 		return( $output );
 	}
@@ -94,22 +96,21 @@ class LudwigMarkdown
 
 		// Find all Markdown blocks
 		// and replace them with converted HTML5
-		preg_match_all( "|<pre id=\"markdown\">(.*)</pre>|Uis", $this->get_markdown( $output ), $matches );
+		$output= $this->get_markdown( $output );
+		preg_match_all( "|<pre id=\"markdown\">(.*)</pre>|Uis", $output, $matches );
 		foreach( $matches[0] as $key => $val )
 		{
-			$text = $md->defaultTransform($matches[1][$key]);
-			$output = str_replace($val,$text,$output);
+			$output = str_replace( $val, $md->defaultTransform($matches[1][$key]), $output );
 		}
 
 		// Add id to h1-h6
 		$output= preg_replace_callback(	"#<h([1-6])>(.*)<\/h[1-6]>#Usi",
-													function($m) {
-														$s= array('+', '.');
-														$r= array('-', '');
-														return '<h'. $m[1] .' id="'. str_replace($s, $r, urlencode(strtolower(strip_tags($m[2])))) .'">'. $m[2] .'</h'. $m[1] .'>';
-													},
-													$output );
-
+						function($m) {
+							$s= array('+', '.');
+							$r= array('-', '');
+							return '<h'. $m[1] .' id="'. str_replace($s, $r, urlencode(strtolower(strip_tags($m[2])))) .'">'. $m[2] .'</h'. $m[1] .'>';
+						},
+						$output );
 		return( $output );
 	}
 
@@ -120,27 +121,27 @@ class LudwigMarkdown
 	{
 		$l= (count($matches) == 3) ? $matches[1] : '';
 		$src= (count($matches) == 3) ? $matches[2] : $matches[1];
-	
+
 		if ($l != '')
 		{
 			require_once( dirname(__FILE__) .'/geshi/geshi.php' );
 			$geshi = new GeSHi( html_entity_decode($src, ENT_QUOTES), $l);
-	
+
 			// Options
 			$geshi->enable_keyword_links(false);
 			$geshi->enable_classes(true);
 			$geshi->enable_line_numbers(GESHI_FANCY_LINE_NUMBERS);
 			$geshi->set_header_type(GESHI_HEADER_NONE);
 			$geshi->set_tab_width(4);
-	
+
 			// Return Code
 			return(	'<div class="code" itemscope itemtype="http://schema.org/Code">'. 
-							'<div itemprop="sampleType">'. $geshi->parse_code() .'</div>'.
-							'<div class="hideme" itemprop="programmingLanguage">'. $l .'</div>'.
-						'</div>' );
-	
+				'<div itemprop="sampleType">'. $geshi->parse_code() .'</div>'.
+				'<div class="hideme" itemprop="programmingLanguage">'. $l .'</div>'.
+				'</div>' );
+
 		}
-	
+
 		return( '<pre class="nocode"><code>'. $src .'</code></pre>' );
 	}
 
@@ -149,21 +150,21 @@ class LudwigMarkdown
 	public function generate_geshi( $output )
 	{
 		# Geshiloader
-		$search= array(	"#<pre class=\"(.*)\"><code>(.*)<\/code></pre>#Uis",		// Pandoc
-								"#<pre><code>(.*)<\/code><\/pre>#Uis",							// Pandoc and PHP-Markdown
-								"#<pre><code class=\"(.*)\">(.*)<\/code></pre>#Uis" );	// PHP-Markdown
+		$search= array(	"#<pre class=\"(.*)\"><code>(.*)<\/code></pre>#Uis",	// Pandoc
+				"#<pre><code>(.*)<\/code><\/pre>#Uis",			// Pandoc and PHP-Markdown
+				"#<pre><code class=\"(.*)\">(.*)<\/code></pre>#Uis" );	// PHP-Markdown
 		foreach ( $search as &$item )
 		{
 			if( preg_match($item, $output) )
 			{
-				$output= preg_replace_callback( $item, 'geshiloader', $output );
+				$output= preg_replace_callback( $item, 'self::geshiloader', $output );
 			}
 		}
 
 		// Responsible Table 
 		// Add special div container before and after the table
 		$output = str_replace(	array('<table', '</table>', '<table>'),
-										array('<div class="resptable"><table', '</table></div>', '<table class="narrow">'), $output);
+					array('<div class="resptable"><table', '</table></div>', '<table class="narrow">'), $output);
 
 		return( $output );
 	}
