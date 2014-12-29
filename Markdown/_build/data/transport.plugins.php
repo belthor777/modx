6aa7e3fdfd09af4b7db32eb5ca317a32c65257cb
gitolite <test@ludwig.im>
2014-12-29 02:54:55 +0100

    Initial LudwigMarkdown

--- Markdown/_build/data/transport.plugins.php
+++ Markdown/_build/data/transport.plugins.php
@@ -33,7 +33,7 @@ $plugins[$i]->fromArray(array(
 // Add Events for Plugin
 $events[0]= $modx->newObject('modPluginEvent');
 $events[0]->fromArray(array(
-	'event' => 'OnWebPageComplete',
+	'event' => 'OnWebPagePrerender',
 	'priority' => 0,
 	'propertyset' => 0
 ),'',true,true);
