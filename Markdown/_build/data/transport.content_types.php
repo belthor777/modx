78f1b1b47bb16ec18638b84132d0c29cc63ed3ef
gitolite <test@ludwig.im>
2014-12-31 18:20:57 +0100

    Added Markdown Content Type

--- Markdown/_build/data/transport.content_types.php
+++ Markdown/_build/data/transport.content_types.php
@@ -8,6 +8,7 @@ $contenttype = array();
 // Source: https://code.google.com/p/modx-revo-ja/source/browse/trunk/_build/data/transport.core.content_types.php?spec=svn30&r=30
 $contenttype['markdown']= $modx->newObject('modContentType');
 $contenttype['markdown']->fromArray(array (
+  'id' => 20,
   'name' => 'Markdown',
   'description' => 'Markdown content',
   'mime_type' => 'text/x-markdown',
