f07f66f0fe2d1e45594a754490db068ff8b28b81
gitolite <test@ludwig.im>
2014-12-31 18:01:14 +0100

    Added Markdown Content Type

---
+++ Markdown/_build/data/transport.content_types.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @package ludwigmarkdown
+ * @subpackage build
+ */
+$contenttype = array();
+
+// Source: https://code.google.com/p/modx-revo-ja/source/browse/trunk/_build/data/transport.core.content_types.php?spec=svn30&r=30
+$contenttype['markdown']= $modx->newObject('modContentType');
+$contenttype['markdown']->fromArray(array (
+  'name' => 'Markdown',
+  'description' => 'Markdown content',
+  'mime_type' => 'text/x-markdown',
+  'file_extensions' => '.md',
+  'headers' => 'NULL',
+  'binary' => 0,
+), '', true, true);
+
+return $contenttype;
