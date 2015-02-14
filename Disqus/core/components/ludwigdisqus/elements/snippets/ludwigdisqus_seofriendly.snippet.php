dcaa43d3c04ae0de54fff037a9ac458a8e02b88c
gitolite <test@ludwig.im>
2015-02-14 17:16:24 +0100

    Optimized Disqus

--- Disqus/core/components/ludwigdisqus/elements/snippets/ludwigdisqus_seofriendly.snippet.php
+++ Disqus/core/components/ludwigdisqus/elements/snippets/ludwigdisqus_seofriendly.snippet.php
@@ -1,3 +1,4 @@
+<?php
 /**
  * ludwigdisqus_seofriendly
  * Copyright 2014 by Thomas Ludwig <thomas@ludwig.im>
@@ -20,64 +21,64 @@
 /**
  * ludwigdisqus_seofriendly
  *
- * Optimize Disqus Comments for Search Engines 
+ * Optimize Disqus Comments for Search Engines
  *
  * @version 1.0
  * @author Thomas Ludwig <thomas@ludwig.im>
  * @copyright Copyright &copy; 2014
  * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU General Public License
- * version 2 or (at your option) any later version for ludwigdisqus sourcecode
+ *          version 2 or (at your option) any later version for ludwigdisqus sourcecode
  * @license http://www.apache.org/licenses/LICENSE-2.0 Apache License
- * version 2 or (at your option) any later version for Disqus-PHP API
+ *          version 2 or (at your option) any later version for Disqus-PHP API
  * @package ludwigdisqus
  */
 
 /*
-[[!ludwigdisqus_seofriendly]]
-*/
-$properties =& $scriptProperties;
+ * [[!ludwigdisqus_seofriendly]]
+ */
+$properties = & $scriptProperties;
 
 // Variables
 $pak = 'ludwigdisqus';
-$properties['chunk'] = (isset($_GET[$properties['chunk']]) ? $_GET[$properties['chunk']] : (!empty($chunk)) ? $chunk : $pak .'_seofriendly');
-$activated = array(	'plugin' => $modx->getOption( $pak .'.activated'),
-					'seofriendly' => $modx->getOption( $pak .'.seofriendly') );
+$properties['chunk'] = ( isset( $_GET[$properties['chunk']] ) ? $_GET[$properties['chunk']] : ( !empty( $chunk ) ) ? $chunk : $pak . '_seofriendly' );
+$activated = array(
+	'plugin' => $modx->getOption( $pak . '.activated' ), 
+	'seofriendly' => $modx->getOption( $pak . '.seofriendly' )
+);
 $output = '';
 
 // Check if package is installed and activated
-$ldpath = MODX_CORE_PATH.'components/'. $pak .'/model/';
-if(!$modx->addPackage($pak, $ldpath, 'disqus_') || !$activated['plugin'] ) 
+$ldpath = MODX_CORE_PATH . 'components/' . $pak . '/model/';
+if ( !$modx->addPackage( $pak, $ldpath, 'disqus_' ) || !$activated['plugin'] )
 {
-	return '';
-
-// Load needed lib API disqus-php
-} elseif ($activated['seofriendly']) {
-
-	$modx_id = $modx->resource->get('id');
-	$res = $modx->query("SELECT t.modx_id, c.author_name, c.comment, c.created, c.like, c.dislike, c.parent_id, c.isDeleted ".
-						"FROM modx.disqus_comment c, modx.disqus_thread t ".
-						"WHERE t.id=c.thread_id AND t.modx_id=". intval($modx_id) );
-	if (is_object($res)) 
+	return ( '' );
+	
+	// Load needed lib API disqus-php
+} elseif ( $activated['seofriendly'] )
+{
+	
+	$modx_id = $modx->resource->get( 'id' );
+	$res = $modx->query( "SELECT t.modx_id, c.author_name, c.comment, c.created, c.like, c.dislike, c.thread_id, c.id as comment_id, c.parent_id, c.isDeleted " . "FROM modx.disqus_comment c, modx.disqus_thread t " . "WHERE t.id=c.thread_id AND t.modx_id=" . intval( $modx_id ) );
+	if ( is_object( $res ) )
 	{
-		$data = $res->fetchAll(PDO::FETCH_ASSOC);
-
-		foreach ($data as $d) 
+		$data = $res->fetchAll( PDO::FETCH_ASSOC );
+		foreach( $data as $d )
 		{
-			if (!$d['isDeleted'])
+			if ( !$d['isDeleted'] )
 			{
-			  $output .= $modx->getChunk($properties['chunk'], array(
-				  'author_name' => $d['author_name'],
-				  'comment' => nl2br($d['comment']),
-				  'created' => $d['created'],    // Format should be 2012-05-18T16:22Z
-				  'like' => $d['like'],
-				  'dislike' => $d['dislike'],
-				  'parent_id' => $d['parent_id']
-			  ));
+				$output .= $modx->getChunk( $properties['chunk'], array(
+					'author_name' => $d['author_name'], 
+					'comment' => nl2br( htmlspecialchars( $d['comment'], ENT_QUOTES ) ), 
+					'created' => $d['created'],  // Format should be 2012-05-18T16:22Z
+					'like' => $d['like'], 
+					'dislike' => $d['dislike'], 
+					'parent_id' => $d['parent_id'], 
+					'comment_id' => $d['comment_id'], 
+					'thread_id' => $d['thread_id']
+				) );
 			}
 		}
-		 
 	}
-
 }
 
-return $output;
+return ( $output );
